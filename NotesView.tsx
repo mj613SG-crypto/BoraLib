b9f47a73ce211427e329276d2a31fe283fe9ce80
mj613SG-crypto <mj613avil@gmail.com>
2026-08-26 23:58:20 -0300

    Update NotesView.tsx

--- NotesView.tsx
+++ NotesView.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useMemo } from 'react';
 import { Highlight, Bookmark, Book } from '../types';
-import { HIGHLIGHT_PRESETS, getHighlightStyle } from '../lib/highlightColors';
+import { HIGHLIGHT_PRESETS, getHighlightStyle } from '../highlightColors';
 import {
   BookmarkCheck,
   Search,
