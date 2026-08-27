23dac58084bafe2b5ed4a52b0ae43c68694494c5
mj613SG-crypto <mj613avil@gmail.com>
2026-08-27 00:26:31 -0300

    Update Navbar.tsx

--- Navbar.tsx
+++ Navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BookOpen, Library, BookmarkCheck, Settings, Cloud, RefreshCw, User as UserIcon, Smartphone } from 'lucide-react';
 import { User } from '../types';
-import { usePwaInstall } from '../usePwaInstall';
+import { usePwaInstall } from '../';
 
 interface NavbarProps {
   activeTab: 'library' | 'shelves' | 'notes' | 'settings';
